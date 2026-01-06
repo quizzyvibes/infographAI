5bc83331644b5525cb3194484892df1e2bb739b6
quizzyvibes <kevinkam08@gmail.com>
2026-01-06 23:50:15 +0800

    Update UserProfile.tsx

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -6,6 +6,7 @@ import {
   User as UserIcon, Settings, Grid, Trash2, ZoomIn, 
   Clock, HardDrive, Zap, LogOut, Mail, Calendar, Shield, Crown
 } from 'lucide-react';
+import { InfoTooltip } from './InfoTooltip';
 
 interface UserProfileProps {
   user: User | null;
@@ -219,3 +220,4 @@ export const UserProfile: React.FC<UserProfileProps> = ({
     </div>
   );
 };
+
