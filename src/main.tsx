89dcb6417f16a61884cae715ab889fc6ab99fc9e
quizzyvibes <kevinkam08@gmail.com>
2026-01-06 17:25:42 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,10 @@ import App from '../App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
