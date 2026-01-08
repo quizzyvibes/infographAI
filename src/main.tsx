6654b4eca98922b240d0ccefcadc1621571e972e
quizzyvibes <kevinkam08@gmail.com>
2026-01-08 09:46:03 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from '../App';
-import './index.css';
-import { AuthProvider } from './context/AuthContext';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from '../App' // App is in root
+import './index.css' // index.css is in src
+import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -10,4 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
     </AuthProvider>
   </React.StrictMode>,
-);
+)
+
