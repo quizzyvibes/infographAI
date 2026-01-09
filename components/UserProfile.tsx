752cb4a3aab21e3d30199109819e7fe01b4e5102
quizzyvibes <kevinkam08@gmail.com>
2026-01-09 19:27:03 +0800

    Update UserProfile.tsx

--- components/UserProfile.tsx
+++ components/UserProfile.tsx
@@ -1,7 +1,6 @@
 
 import React, { useState } from 'react';
-import { User } from 'firebase/auth';
-import { HistoryItem } from '../src/types'; 
+import { HistoryItem, AppUser } from '../src/types'; 
 import { 
   User as UserIcon, Settings, Grid, Trash2, ZoomIn, 
   Clock, HardDrive, Zap, LogOut, Mail, Calendar, Shield, Crown
@@ -9,7 +8,7 @@ import {
 import { InfoTooltip } from './InfoTooltip';
 
 interface UserProfileProps {
-  user: User | null;
+  user: AppUser | null;
   history: HistoryItem[];
   onLoadHistory: (item: HistoryItem) => void;
   onDeleteHistory: (id: string, path: string | undefined, e: React.MouseEvent) => void;
@@ -220,6 +219,7 @@ export const UserProfile: React.FC<UserProfileProps> = ({
     </div>
   );
 };
+
 
 
 
